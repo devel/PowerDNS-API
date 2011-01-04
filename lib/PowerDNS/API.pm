2e00d6054f445aed49f0f0e1d64ca256d432f1cc
Ask Bjørn Hansen <ask@develooper.com>
2011-01-03 21:24:45 -0800

    dzil new

---
+++ lib/PowerDNS/API.pm
@@ -0,0 +1,51 @@
+package PowerDNS::API;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+PowerDNS::API -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over4
+
+=item ...
+
+=back
+
+=head1 AUTHOR
+
+Ask Bjørn Hansen, C<< <ask at develooper.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to the issue tracker at
+L<http://github.com/abh/PowerDNS-API/issues>.
+
+The Git repository is available at
+L<http://github.com/abh/PowerDNS-API>
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 Ask Bjørn Hansen, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+
+=cut
