eab77d3043f15a706e9f665bd4799cac178fe6f8
Ask Bjørn Hansen <ask@develooper.com>
2011-02-06 01:25:22 -0800

    Parse SOA records when serializing

--- lib/PowerDNS/API/Schema/Role/Record.pm
+++ lib/PowerDNS/API/Schema/Role/Record.pm
@@ -1,6 +1,15 @@
 package PowerDNS::API::Schema::Role::Record;
 use Moose::Role;
 
+# http://doc.powerdns.com/types.html
+
+sub _parse_soa {
+    my $self = shift;
+    my %data;
+    @data{qw(primary hostmaster serial refresh retry expire default_ttl)} = split /\s+/, $self->content;
+    return \%data;
+}
+
 sub TO_JSON {
     my $self = shift;
 
@@ -10,9 +19,17 @@ sub TO_JSON {
                };
     
     my $domain = $self->domain->name;
-    
+   
     $data->{name} =~ s{\Q$domain\E$}{};
     $data->{name} =~ s{\.$}{};
+
+    my $m = "_parse_" . lc $self->type;
+    if ($self->can($m)) {
+        $data->{data} = $self->$m;
+    }
+    else {
+        $self->{data} = $self->content;
+    }
 
     return $data;
 }
